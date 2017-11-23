8accdc372c03a64f00f3bcbb7fa0ff15ef6d18ff
BongGeek <abhinaba@gmail.com>
2017-11-23 02:57:22 +0000

    added image tagging in build

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ go build ./wordelementservice.go
 
 echo Building image $CONTAINER:$VERSION
 docker build -t $CONTAINER:$VERSION .
+docker tag $CONTAINER:$VERSION $DOCKER_ID_USER/$CONTAINER
+docker tag $CONTAINER:$VERSION bonggeek.azurecr.io/$CONTAINER
 
 echo Done .............................
 echo 
