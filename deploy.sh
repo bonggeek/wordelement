b5da4ca3172d0f50ea6ec001ef61beac92528ccc
BongGeek <abhinaba@gmail.com>
2017-11-28 23:25:10 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker run -d -p 80:8080 bonggeek/elementservice
+
+# curl -fsSL get.docker.com -o get-docker.sh
+# sh get-docker.sh
+
+docker ps -q | xargs docker stop 
+docker pull bonggeek/elementservice
+docker run -d --restart="always" -p 80:8080 bonggeek/elementservice
